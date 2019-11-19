bf48f31678e7d317c87568d9f99dba8e7b0eb850
Venkata Sreekanth Chintada <venkata.sreekanthchintada@nxp.com>
2019-11-19 14:43:45 +0530

    macaque: enum test   Signed-off-by: 'V Sreekanth Chintada <chvsree2007@gmail.com>'

---
+++ enum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+enum color{red,green=5,blue}c;
+int main()
+{
+        cout <<"blue is "<< blue <<endl;
+        cout <<"red is "<< red <<endl;
+        cout <<"green is "<< green <<endl;
+        return 0;
+}       
+
